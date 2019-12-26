311966aecb0f41c0760012f15f84c555ba051211
Chris Ellis <ellis.cs@gmail.com>
2019-12-26 16:04:17 -0500

    Change game to free form

--- src/decode.js
+++ src/decode.js
@@ -8,18 +8,31 @@ import React, { useState, useEffect } from 'react'
         3: 'm',
         4: 'r',
         5: 'l',
-        6: 'jgc',
+        6: 'jg',
         7: 'kcq',
         8: 'fv',
         9: 'pb'
     }
     let broadcast = {};
+    let letterOptions = [];
     for (const [number, letters] of Object.entries(encode)) {
         // console.log(number, letters)
         letters.split('').map(letter => {
+            // console.log(letter, number)
+            letterOptions.push(letter);
             return broadcast[letter] = number;
-        })
+        });
     }
+
+    letterOptions.sort()
+    // console.log(letterOptions)
+    // let numbers = Object.entries(encode)
+    // numbers.map((number) => {
+    //     console.log(number)
+    //     return number;
+    // })
+
+    // console.log(numbers)
 
     const test = [];
     let numbs = '0123456789';
@@ -39,7 +52,7 @@ import React, { useState, useEffect } from 'react'
         })
     });
 
-    console.log(test)
+    // console.log(test)
 
     const getRandomQuestion = (question) => {
         let randomQuestion = Math.floor(Math.random()*test.length).toString()
@@ -56,51 +69,67 @@ import React, { useState, useEffect } from 'react'
 function Decode() {
     
     const [question, setQuestion] = useState('00')
-    const [answers, setAnswers] = useState([])
     const [score, setScore] = useState(0);
+    const [userAnswer, setUserAnswer] = useState('');
     
     useEffect(() => {
-        // console.log(test[question].length)
-        const rightAnswerIndex = Math.floor(Math.random() * Math.floor(test[question].length))
-        const rightAnswer = test[question][rightAnswerIndex];
-        const wrongAnswers = Array.from(test).flat().sort(() => 0.5 - Math.random()).slice(0,3);
-        let ans = []
-        wrongAnswers.forEach((answer) => {
-            ans.push({answer, isAnswer: false})
-        })
-        ans.push({answer: rightAnswer, isAnswer: true})
-        const randomizedAnswers = ans.sort(() => 0.5 - Math.random());
-        // console.log(randomizedAnswers)
-        setAnswers(randomizedAnswers)
-    }, [question]);
+        if(userAnswer.length === 2) {
+            const isCorrect = [];
+            const theAnswer = userAnswer.split('');
+            const theQuestion = question.split('');
 
-    const selectAnswer = ({answer, isAnswer}) => {
-        if(isAnswer) {
-            setScore(score + 1)
-            // select next question
-            let randomQuestion = getRandomQuestion(question);
-            setQuestion(randomQuestion);
+            for (let step = 0; step < 2; step++) {
+                const number = theQuestion[step];
+                const containsNumber = encode[number].includes(theAnswer[step]);
+                isCorrect.push({
+                    number,
+                    containsNumber
+                });
+            }
+
+            // Iterate through theAnswer and reduce to true or false
+           const totallyCorrect = isCorrect.reduce((acc, item) => {
+                return item.containsNumber;
+            }, false);
+
+            if(totallyCorrect) {
+                setScore(score + 1);
+                let randomQuestion = getRandomQuestion(question);
+                setUserAnswer('');
+                setQuestion(randomQuestion);
+            }
+        }
+    }, [question, userAnswer, score])
+
+    const selectLetter = ({letter}) => {
+        if(userAnswer === undefined || userAnswer.length < 2) {
+            return setUserAnswer(userAnswer + letter)
         }
     }
 
     return (
         <>
-            <div className="w-1/3 rounded shadow-lg bg-white">
+            <div className="w-full text-gray-500 justify-center text-center text-6xl">
+                {score}
+            </div>
+            <div className="flex sm:flex-shrink-0 rounded shadow-lg bg-white">
                 <div className="px-6 py-6">
-                    <div className="w-full text-center px-6 py-4 font-bold text-x1 mb-2">
+                    <div className="w-full text-center px-6 py-4 font-bold text-5xl mb-2">
                         {question}
-                    </div>
-                    <div className="flex flex-wrap justify-center">
-                        {answers.map((answer) => {
-                            return (
-                                <button className="w-1/3 mx-1 my-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" key={answer.answer} onClick={() => selectAnswer(answer)}>{answer.answer}</button>
-                            )
-                        })}
+                        <br />
+                        {userAnswer}
                     </div>
                 </div>
             </div>
-            <div className="w-full text-gray-500 justify-center text-center text-6xl">
-                {score}
+            <div className="w-full">
+                <div className="flex flex-wrap justify-center">
+                    {letterOptions.map((letter) => {
+                        return (
+                            <button className="w-1/6 mx-1 my-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" key={letter} onClick={() => selectLetter({letter})} >{letter}</button>
+                        );
+                    })}
+                    <button className="w-full mx-1 my-1 bg-transparent hover:bg-red-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded" onClick={() => setUserAnswer('')} >Clear</button>
+                </div>
             </div>
         </>
     )
